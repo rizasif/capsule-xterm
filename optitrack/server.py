4e00ba66470f16c37a799cd0dcf847b4628450d5
Rizwan Asif <rizasif@gmail.com>
2018-06-14 18:02:02 +0500

    eular bug

--- optitrack/server.py
+++ optitrack/server.py
@@ -16,20 +16,20 @@ def receiveNewFrame( frameNumber, markerSetCount, unlabeledMarkersCount, rigidBo
 
 # This is a callback function that gets connected to the NatNet client. It is called once per rigid body per frame
 def receiveRigidBodyFrame( id, position, rotation ):
+    quart = Quaternion(rotation[0], rotation[1], rotation[2], rotation[3])
+    euler = quart.euler
     if debug:
         print( "Received frame for rigid body", id )
         print( "Position: ", position )
         logging.info( "Position: {}".format(position))
-        # print( "Rotation: ", rotation)
-        quart = Quaternion(rotation[0], rotation[1], rotation[2], rotation[3])
-        print( "Rotation: ", quart.euler)
-        logging.info( "Rotation: {}".format(quart.euler))
+        print( "Rotation: ", euler)
+        logging.info( "Rotation: {}".format(euler))
     data[0] = position[0]
     data[1] = position[1]
     data[2] = position[2]
-    data[3] = rotation[0]
-    data[4] = rotation[1]
-    data[5] = rotation[2]
+    data[3] = euler[0]
+    data[4] = euler[1]
+    data[5] = euler[2]
 
 def StartServer():
     # This will create a new NatNet client
